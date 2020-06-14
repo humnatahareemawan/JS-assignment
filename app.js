b0d1d44602f8faaa9be3860592858063bf62b596
humnatahareemawan <65032454+humnatahareemawan@users.noreply.github.com>
2020-06-14 17:37:10 +0500

    JS assignment

---
+++ app.js
@@ -0,0 +1,334 @@
+// ========= chapter 1 task 1
+
+// alert("welcome to javascript assignment")
+
+// ==========chapter1 task 2
+//  alert("Error!Please enter a valid password")
+
+//============ chapter1 task 3 
+// alert("Welcome to js land....\n Happy coding!")
+
+// ===========chapter1 task 4
+
+// alert("Welcome to js land....") 
+
+// 		alert("Happy Coding \n prevent this page from additional dialog box" ) 
+
+// =========chapter1 task 5
+
+// alert("Hello... I can run JS through my console")
+
+// =========chapter1 task 6
+// {/* <script>
+// document.getElementById("demo").innerHTML = 5 + 6;
+// </script> */}
+
+// ==============chapter2 task 1
+// var name = "username"
+
+// ==============chapter2 task 2
+// var name = "myname"
+// var x = "humna" + " " + "awan"
+
+// ==============chapter2 task 3
+
+// var message = "Hellow world"
+// alert(message);
+
+// ==============chapter2 task 4
+
+// var name = "humna"
+// alert(name);
+// var age = "16 years old"
+// alert(age)
+// var qualification ="certified mobile application development"
+// alert(qualification)
+
+// ==============chapter2 task 5
+
+// var a = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(a)
+
+// ==============chapter2 task 6
+// var email = "example" + "@" + "example.com"
+// alert(email)
+
+// ==============chapter2 task 7
+// var book = "A smarter way to learn JavaScript"
+// alert(book)
+
+
+// ==============chapter2 task 8
+
+// document.write("YAH i can writ document through java script")
+
+// ==============chapter2 task 9
+
+// var a ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(a)
+
+// ==============chapter3 task 1
+
+// var age = "I am 16 years old"
+// alert(age)
+
+// ==============chapter3 task 2
+
+// var a ="You have visited this site 10 times"
+// alert(a)
+
+// ==============chapter3 task 3
+
+// var birthyear = "my birth year is 2001 \n date type variable is number"
+// document.write(birthyear)
+
+// ==============chapter3 task 4
+// var Visitorsname ="humna"
+// var Product  = "shirts"
+// var  Quantity ="5"
+// document.write( Visitorsname +  "   " +  "ordered" + Quantity + "  "  + Product + "on XYZ Clothing store") 
+
+// ===============chapter4 task 1
+
+// var a,b,c
+// var a = humna , b = tahreem , c = awan
+
+
+// ===============chapter4 task 2
+
+//  var a,b,c,d,e
+//  var a = "humna" , b = "1", c = "$", d = "_h" , e = "awais"
+
+//  var f,g,h,i,j
+//  var f =hgy , g = 1 , h = 0, i = _2 , j = 4
+//  <h1> Rules for naming JS variables</h1>
+
+
+//  document.write("Variable" + "names " +  "can " + "only "   +   "contain"    +  "" 
+//  +a+ ""+ b +""  + c +""+d +""+e)
+//  document.write("Variable" + "names " +  "can " + "only "   +   "contain"    +  "" 
+//  +f+ ""+ g +""  + h +""+d  + "" + i + "" +""+j)
+//  document.write("Variable" + "names " +  "must  " + "begain  "   +   "with"    +  "CApital letter")
+//  document.write("Variable" + "names " +  "are " + "case  "   +   "sensitive"    )
+//  document.write("Variable" + "names " +  "shouldnot " + "be  " + "a  " + "JS   " + "Variable  "   )
+
+// ===============chapter5 task 1
+// var a = 3
+// var b = 5
+// var sum = a + b
+// document.write("sum" +  "   " +  "of" + "   " + a + "  "  + "and" +"  " + b +"  "+  "is"+ sum)
+
+
+// ===============chapter5 task 2
+// var a = 3
+// var b = 5
+// var minus = a - b
+// document.write("sum" +  "   " +  "of" + "   " + a + "  "  + "and" +"  " + b +"  "+  "is"+ minus)
+
+// var a = 3
+// var b = 5
+// var mul = a * b
+// document.write("sum" +  "   " +  "of" + "   " + a + "  "  + "and" +"  " + b +"  "+  "is"+ mul)
+
+// var a = 3
+// var b = 5
+// var divide = a / b
+// document.write("sum" +  "   " +  "of" + "   " + a + "  "  + "and" +"  " + b +"  "+  "is"+ divide)
+
+// var a = 3
+// var b = 5
+// var mod = a % b
+// document.write("sum" +  "   " +  "of" + "   " + a + "  "  + "and" +"  " + b +"  "+  "is"+ mod)
+
+// ===============chapter5 task 3
+// var num = 5
+// document.write("initial" +  "   " +  "variable" +  "  "  + "is" +"  " + a )
+
+ 
+// var newNum = num++;
+// document.write("increament" +  "   " +  "variable" +  "  "  + "is" +"  " + newNum)
+
+// var y = newNum + 7;
+// document.write("addition" +  "   " +  "variable" +  "  "  + "is" +"  " + y)
+
+// var d = y--;
+// document.write("decreament" +  "   " +  "variable" +  "  "  + "is" +"  " + d)
+
+// var f = d % y;
+// document.write("remainder " +  "   " +  "is"  +"  " + f)
+
+
+// ===============chapter5 task 4
+ 
+// var a = 600
+// var b = 600 * 5
+// document.write("price of " +  "   " +  "5 tickets" +  "  "  + "is" +"  " + b)
+
+// ===============chapter5 task 5
+// var a = 4
+// var b1 = a * 1
+// var b2 = a * 2 
+// var b3  = a * 3 
+// var b4 = a * 4
+// var b5 = a * 5 
+// var b6 = a * 6 
+// var b7 = a * 7 
+// var b8 = a * 8 
+// var b9 = a * 9 
+// var b10 =a * 10
+
+// document.write(a +  "   " +  "X" +  "  "  + "1" +"  " + "=" + b1) 
+// document.write(a +  "   " +  "X" +  "  "  + "2" +"  " + "=" + b2)
+// document.write(a +  "   " +  "X" +  "  "  + "3" +"  " + "=" + b3)
+// document.write(a +  "   " +  "X" +  "  "  + "4" +"  " + "=" + b4)
+// document.write(a +  "   " +  "X" +  "  "  + "5" +"  " + "=" + b5)
+// document.write(a +  "   " +  "X" +  "  "  + "6" +"  " + "=" + b6)
+// document.write(a +  "   " +  "X" +  "  "  + "7" +"  " + "=" + b7)
+// document.write(a +  "   " +  "X" +  "  "  + "8" +"  " + "=" + b8)
+// document.write(a +  "   " +  "X" +  "  "  + "9" +"  " + "=" + b9)
+// document.write(a +  "   " +  "X" +  "  "  + "10" +"  " + "=" + b10)
+
+// ===============chapter5 task 5
+// var C = 25 
+// var F = 120
+// var Celcius = (F - 32) *5 / 9 
+// var Fehrinhiet = (C * 9 / 5) + 32 
+// document.write(F +  "   " +  "is" +  "  "  + Celcius )
+// document.write(C +  "   " +  "is" +  "  "  + Fehrinhiet )
+
+// ===============chapter5 task 6
+// var a = 650
+// var b = 400
+// var Quantity1 = 2
+// var Quantity2 = 3
+// var shipping = 100
+// var total =  (a * Quantity1) + (b * Quantity2) + shipping
+// document.write("price " +  "   " +  "of" +  "  "  + "1 item" +" " + "is" + a) 
+// document.write("quantity " +  "   " +  "of" +  "  "  + "1 item" +" " + "is" + Quantity1)
+// document.write("price " +  "   " +  "of" +  "  "  + "2 item" +" " + "is" + b)
+// document.write("quantity " +  "   " +  "of" +  "  "  + "1 item" +" " + "is" + Quantity2)
+// document.write("Shipping " +  "   " +  "charges" +  "  "  + "is" + shipping)
+// document.write("total"  +  "   " +  "price" +  "  "  + "of" +" "+"your" +" "+"order" +" " + "is" + total)
+
+// ===============chapter5 task 6
+ 
+// var totalmarks = 400
+// var obtainedmarks = 100
+// var percentage = (obtainedmarks / totalmarks) * 100
+// document.write("total " +  "   " +  "marks"  +" " + "is" + totalmarks)
+
+// document.write("obtained " +  "   " +  "marks"  +" " + "is" + obtainedmarks)
+
+// document.write("percentage "  + "is" + percentage)
+
+// =====================chapter6-9 task 1
+// var a = 10
+// var b = a++
+// var c = ++a
+// var d = a--
+// var e = --a
+
+// document.write("the " +  "   " +  "value"  +" " + "of" + "a" + "is" +a)
+// document.write("the " +  "   " +  "value"  +" " + "of" + "  "  + "increament" + "  " +"is" +b)
+
+// document.write("the " +  "   " +  "value"  +" " + "of" + "  "  + "++a"+ "is" +c)
+// document.write("the " +  "   " +  "value"  +" " + "of" + "  "  + "a--"+ "is" +d)
+// document.write("the " +  "   " +  "value"  +" " + "of" + "  "  + "--a"+ "is" +e)
+
+// =====================chapter6-9 task 2
+// var a = 2
+// var b = 1
+// var result = --a - --b + ++b + b--
+// document.write("a " +  "   " +  "is"  +" " + a )
+// document.write("a " +  "   " +  "is"  +" " + b )
+// document.write("a " +  "   " +  "is"  +" " + result)
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
